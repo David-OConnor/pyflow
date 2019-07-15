e7b902878a25ffdff8b8ee529bb4c1c235bfc00a
David O'Connor <david.alan.oconnor@gmail.com>
2019-07-15 15:45:14 +0400

    More setup work

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Py Packages
 
-This library attempts to implement [PEP 582 -- Python local packages directory]
-(Python local packages directory). It abstracts over commands used for creating
+This tool attempts to implement 
+[PEP 582 -- Python local packages directory](Python local packages directory). It abstracts over commands used for creating
 activating, modifying, and using virtual environments.
 
 
@@ -10,17 +10,36 @@ There are 2 main ways to install this:
 - Download the binary from (fill in), and add it to a location on the system path.
 For example, place it under `/usr/bin` in linux, or `~\AppData\Local\Programs\Python\Python37\bin` in Windows.
 - If you have `Rust` installed, the most convenient way is to 
-run `cargo install pypackages`. If you don't, this will result in a large download
-you may not want.
+run `cargo install pypackages`.
+
 
 ## Use
-Create the file `Python.toml` in your project directory.
+- Create the file `Python.toml` in your project directory
+
+Example contents:
+```toml
+[Python]
+version = "3.7"
+
+[dependencies]
+numpy = "^1.16.4"
+django = "^2.0.0"
+```
+- In a terminal, Run `pyprojects install` to install all dependencies in the `Python.toml`.
+- Run `pyprojects` followed by the command you wish in order to run that
+command inside the virtual environment. Eg `pyprojects python main.py`.
+
+Additional commands:
+- `pyprojects install numpy`: Install a specific package (`numpy` in this example), and add it to `Python.toml`.
+- `pyprojects uninstall numpy`. Uninstall a package, and remove it from `Python.toml`.
+
 
 ## Why?
 
 Using the main python installation, especially one the operating system depends on
 isn't a good way to use python packages. Virtual environments isolate dependencies
-for each project, but are cumbersome to use. An example workflow:
+for each project, and prevent damaging system python setups,
+but are cumbersome to use. An example workflow:
 
 Setup:
 ```bash
@@ -40,6 +59,12 @@ cd ~/myproject
 python main.py
 deactivate
 ```
+This is a signifcant impact the usability of Python, especially for new users. IDEs like `PyCharm` abstract this away, but are a specific solution
+to a general problem.
+
+If [PEP 582](Python local packages directory) is impelemented, this tool
+will become obsolete, but this isn't likely to happen in the near future.
+
 
 ## Why use this over existing projects
 Pipenv and Poetry both address this problem directly. This project aims to keep
@@ -47,19 +72,35 @@ dependencies in the project directory, both in `__pypackages__`, and a `.venv`
 folder, containing the virtual environment. It doesn't modify files outside
 your project directory. It aims to be simple, fast, and easy to use.
 
+
 ## What this doesn't do (at least currently)
 
 - Lock dependencies
 - Check or resolve dependency conflicts
 
 
-## Why Rust over Python
-We'd like to avoid the ambiguity of which Python version is running commands
-from this library, by providing a binary available on the system path. The downside
+## Why Rust over Python?
+We'd like to avoid the ambiguity of which Python version is executing commands
+from this tool by providing a standalone binary. Eg, as an executable
+python script, we may run into ambiguity over which python version is activating
+the environment and installing dependencies. (System Python 2? Python 3? A different
+version of Python 3? A user-installed Python? Super-user Python?) 
+
+A downside
 to this approach is that contributing to this project may be less accessible
 to its users.
 
 
+## Why create a new config format?
+In the future, this tool may adopt or consolidate with
+[pyproject.toml](https://poetry.eustace.io/docs/pyproject/) or
+[Pipfile](https://github.com/pypa/pipfile), but we're starting
+with a custom config.
+
+`requirements.txt` doesn't include useful metadata, like the required Python version.
+
+
 ## Gotchas
-- Make sure the `pypackages` binary is accessible in your path.
+- Make sure the `pypackages` binary is accessible in your path. If installing
+via `Cargo`, this should be set up automatically.
 - Make sure `__pypackages__` and `.venv` are in your `.gitignore` file.
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,114 @@
 // use dirs;
 use regex;
-use std::{env, fs, process::Command};
+use std::{env, fs, num, path, process::Command, str};
 use structopt::StructOpt;
 use toml;
+
+#[derive(Debug)]
+enum Arg {
+    Install,
+    Uninstall,
+    Custom(String), // eg python
+}
+
+impl str::FromStr for Arg {
+    type Err = num::ParseIntError; // todo not sure what to put here.
+
+    fn from_str(arg: &str) -> Result<Self, Self::Err> {
+        let result = match arg {
+            "install" => Arg::Install,
+            "uninstall" => Arg::Uninstall,
+            _ => Arg::Custom(arg.into()),
+        };
+
+        Ok(result)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum VersionType {
+    Exact,
+    OrHigher,
+}
+
+impl ToString for VersionType {
+    fn to_string(&self) -> String {
+        match self {
+            VersionType::Exact => "==".into(),
+            VersionType::OrHigher => ">=".into(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Package {
+    name: String,
+    version: (u32, u32, u32), // https://semver.org
+    version_type: VersionType,
+}
+
+impl Package {
+    pub fn name_with_version(self) -> String {
+        self.name
+            + &self.version_type.to_string()
+            + &format!("{}.{}.{}", self.version.0, self.version.1, self.version.2)
+    }
+}
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "basic")]
 struct Opt {
-    #[structopt(short = "d", long = "debug")]
-    debug: bool,
+    #[structopt(name = "args")]
+    args: Vec<Arg>, // ie "install", "python" etc.
 }
 
-/// Parse the version from a config file, eg "3.7".
-fn version_from_str(version: &str) -> (u32, u32) {
+struct Config {
+    py_version: (u32, u32),
+    dependencies: Vec<Package>,
+}
+
+impl Config {
+    pub fn from_file(file_name: &str) -> Self {
+        match fs::read_to_string(file_name) {
+            Ok(data) => {
+                let data = data
+                    .parse::<toml::Value>()
+                    .expect("Problem parsing Python.toml");
+
+                let version_str = &data["Python"]["version"]
+                    .as_str()
+                    .expect("Problem finding py_version in Python.toml");
+                let py_version = py_version_from_str(version_str);
+
+                let mut dependencies = Vec::new();
+                dependencies.push(Package {
+                    name: "saturn".into(),
+                    version: (0, 3, 5),
+                    version_type: VersionType::OrHigher,
+                });
+                // todo fill dependencies
+
+                Self {
+                    py_version,
+                    dependencies,
+                }
+            }
+            Err(_) => panic!("Can't find Python.toml in this directory . Does it exist?"),
+        }
+    }
+}
+
+/// Parse the py_version from a config file, eg "3.7".
+fn py_version_from_str(py_version: &str) -> (u32, u32) {
     (3, 7)
 }
 
-/// Find the version from the `python --version` command. Eg: "Python 3.7".
+/// Find the py_version from the `python --py_version` command. Eg: "Python 3.7".
 fn find_version(alias: &str) -> (u32, u32) {
     Command::new(alias)
-        .args(&["--version"])
+        .args(&["--py_version"])
         .output()
-        .expect("Problem finding python version");
+        .expect("Problem finding python py_version");
 
     // todo fix with regex
     (3, 7)
@@ -37,47 +124,75 @@ fn find_py_alias(config_ver: (u32, u32)) -> String {
 }
 
 /// Create the virtual env
-fn create_venv(py_alias: String) {
+fn create_venv(py_alias: &str, venv_path: &path::PathBuf) {
     // let home = dirs::home_dir().expect("Can't find home");
-    let project_dir = env::current_dir().expect("Can't find current path");
+
     // let venvs_path = project_dir.join(".virtualenvs");
     // let venv_path = home.join(format!(".virtualenvs/{}", env_name));
-    let venv_path = project_dir.join(".venv");
 
-    if !venv_path.exists() {
-        // fs::create_dir(venv_path).expect("Problem creating virtual env folder");
-        Command::new(&py_alias)
+    // fs::create_dir(venv_path).expect("Problem creating virtual env folder");
+    Command::new(py_alias)
         // .current_dir(venv_path)
         .args(&["-m", "venv", ".venv"])
         .spawn()
-        .expect("Problem creating virtual environment");
+        .expect("Problem creating the virtual environment");
+}
+
+fn venv_exists(venv_path: &path::PathBuf) -> bool {
+    venv_path.exists()
+}
+
+fn activate_venv(venv_path: &path::PathBuf) {
+    Command::new("source")
+        // todo: path to activate may be diff on differnet OSes
+        .current_dir(venv_path)
+        .args(&["bin/activate"])
+        .spawn()
+        .expect("Problem activating the virtual environment");
+}
+
+fn deactivate_venv(venv_path: &path::PathBuf) {
+    // todo: Make this and other errors caused by user input neater; Ie
+    // don't show anything Rust specific.
+    Command::new("deactivate")
+        .spawn()
+        .expect("Problem deactivating the virtual environment");
+}
+
+fn install_all(py_alias: &str, venv_path: &path::PathBuf, packages: &Vec<Package>) {
+    for package in packages {
+        let name = package.clone().name_with_version();
+        let args = vec!["-m", "pip", "install", &name];
+        
+        Command::new("python")
+            .args(&args)
+            .spawn()
+            .expect("Problem installing packages from Python.toml");
     }
 }
 
 fn main() {
-    // let opt = Opt::from_args();
-    // println!("{:?}", opt);
+    let opt = Opt::from_args();
+    let config = Config::from_file("Python.toml");
+    let py_alias = find_py_alias(config.py_version);
+    let project_dir = env::current_dir().expect("Can't find current path");
+    let venv_path = project_dir.join(".venv");
 
-    let mut config_ver = (0, 0);
-    let config = fs::read_to_string("Python.toml");
-    match config {
-        Ok(data) => {
-            let config_data = data
-                .parse::<toml::Value>()
-                .expect("Problem parsing Python.toml");
-            // todo error handle not finding version in toml.
-            let config_version_str = &config_data["Python"]["version"]
-                .as_str()
-                .expect("Problem finding version in Python.toml");
+    for arg in opt.args.iter() {
+        match arg {
+            Arg::Install => {
+                if venv_exists(&venv_path) {
+                    activate_venv(&venv_path);
 
-            config_ver = version_from_str(config_version_str);
+                    // todo: Only if no specific thign specified.
+                    install_all(&py_alias, &venv_path, &config.dependencies);
+
+                    deactivate_venv(&venv_path);
+                } else {
+                    create_venv(&py_alias, &venv_path);
+                }
+            }
+            _ => (),
         }
-        Err(_) => panic!("Can't find Python.toml in this directory . Does it exist?"),
-    };
-  
-
-    let py_alias = find_py_alias(config_ver);
-    create_venv(py_alias);
-
-    
+    }
 }
