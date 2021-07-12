287f0ad83d63fb1e014ef900277bc79131b28750
Matteo Santamaria <matteo@infima.io>
2021-07-11 21:00:31 -0400

    Allow for __python__ variable specification

--- src/script.rs
+++ src/script.rs
@@ -3,10 +3,10 @@ use crate::dep_types::{Constraint, Extras, Lock, Req, ReqType, Version};
 use crate::util;
 use regex::Regex;
 use std::fs;
-use std::io::{BufRead, BufReader};
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use crate::commands;
+use crate::dep_parser::parse_version;
 use std::str::FromStr;
 
 /// Run a standalone script file, with package management
@@ -21,9 +21,10 @@ pub fn run_script(
 ) {
     #[cfg(debug_assertions)]
     eprintln!("Run script args: {:?}", args);
+
     // todo: DRY with run_cli_tool and subcommand::Install
-    let filename = if let Some(a) = args.get(0) {
-        a.clone()
+    let filename = if let Some(arg) = args.get(0) {
+        arg
     } else {
         util::abort(
             "`script` must be followed by the script to run, eg `pyflow script myscript.py`",
@@ -43,7 +44,13 @@ pub fn run_script(
     let cfg_vers;
     let py_vers_path = env_path.join("py_vers.txt");
 
-    if py_vers_path.exists() {
+    let script = fs::read_to_string(filename).expect("Problem opening the Python script file.");
+    let dunder_python_vers = check_for_specified_py_vers(&script);
+
+    if let Some(dpv) = dunder_python_vers {
+        cfg_vers = dpv;
+        create_or_update_version_file(&py_vers_path, &cfg_vers);
+    } else if py_vers_path.exists() {
         cfg_vers = Version::from_str(
             &fs::read_to_string(py_vers_path)
                 .expect("Problem reading Python version for this script")
@@ -52,11 +59,7 @@ pub fn run_script(
         .expect("Problem parsing version from file");
     } else {
         cfg_vers = util::prompt_py_vers();
-
-        fs::File::create(&py_vers_path)
-            .expect("Problem creating a file to store the Python version for this script");
-        fs::write(py_vers_path, &cfg_vers.to_string())
-            .expect("Problem writing Python version file.");
+        create_or_update_version_file(&py_vers_path, &cfg_vers);
     }
 
     // todo DRY
@@ -76,7 +79,7 @@ pub fn run_script(
         cache: dep_cache_path.to_owned(),
     };
 
-    let deps = find_deps_from_script(&PathBuf::from(&filename));
+    let deps = find_deps_from_script(&script);
 
     let lock = match util::read_lock(&lock_path) {
         Ok(l) => l,
@@ -129,15 +132,55 @@ pub fn run_script(
     };
 }
 
-/// Find a script's dependencies from a variable: `__requires__ = [dep1, dep2]`
-fn find_deps_from_script(file_path: &Path) -> Vec<String> {
-    // todo: Helper for this type of logic? We use it several times in the program.
-    let f = fs::File::open(file_path).expect("Problem opening the Python script file.");
+/// Create the `py_vers.txt` if it doesn't exist, and then store `cfg_vers` within.
+fn create_or_update_version_file(py_vers_path: &Path, cfg_vers: &Version) {
+    if !py_vers_path.exists() {
+        fs::File::create(&py_vers_path)
+            .expect("Problem creating a file to store the Python version for this script");
+    }
+    fs::write(py_vers_path, &cfg_vers.to_string()).expect("Problem writing Python version file.");
+}
 
+/// Find a script's Python version specificion by looking for the `__python__` variable.
+///
+/// If a `__python__` variable is identified, the version must have major, minor, and
+/// patch components to be considered valid. Otherwise, there is still some ambiguity in
+/// which version to use and an error is thrown.
+fn check_for_specified_py_vers(script: &str) -> Option<Version> {
+    let re = Regex::new(r#"^__python__\s*=\s*"(.*?)"$"#).unwrap();
+
+    for line in script.lines() {
+        if let Some(capture) = re.captures(&line) {
+            let specification = capture.get(1).unwrap().as_str();
+            let (_, version) = parse_version(specification).unwrap();
+            match version {
+                Version {
+                    major: Some(_),
+                    minor: Some(_),
+                    patch: Some(_),
+                    extra_num: None,
+                    modifier: None,
+                    ..
+                } => return Some(version),
+                _ => {
+                    util::abort(
+                        "Problem parsing `__python__` variable. Make sure you've included \
+                        major, minor, and patch specifications (eg `__python__ = X.Y.Z`)",
+                    );
+                }
+            }
+        }
+    }
+    None
+}
+
+/// Find a script's dependencies from a variable: `__requires__ = [dep1, dep2]`
+fn find_deps_from_script(script: &str) -> Vec<String> {
+    // todo: Helper for this type of logic? We use it several times in the program.
     let re = Regex::new(r"^__requires__\s*=\s*\[(.*?)\]$").unwrap();
 
     let mut result = vec![];
-    for line in BufReader::new(f).lines().flatten() {
+    for line in script.lines() {
         if let Some(c) = re.captures(&line) {
             let deps_list = c.get(1).unwrap().as_str().to_owned();
             let deps: Vec<&str> = deps_list.split(',').collect();
@@ -154,4 +197,43 @@ fn find_deps_from_script(file_path: &Path) -> Vec<String> {
         }
     }
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::dep_types::Version;
+    use crate::script::check_for_specified_py_vers;
+    use rstest::rstest;
+
+    const NO_DUNDER_PYTHON: &str = r#"
+if __name__ == "__main__":
+    print("Hello, world")
+"#;
+
+    const VALID_DUNDER_PYTHON: &str = r#"
+__python__ = "3.9.1"
+
+if __name__ == "__main__":
+    print("Hello, world")
+"#;
+
+    fn py_version() -> Option<Version> {
+        let version = Version {
+            major: Some(3),
+            minor: Some(9),
+            patch: Some(1),
+            extra_num: None,
+            modifier: None,
+            star: false,
+        };
+        Some(version)
+    }
+
+    #[rstest]
+    #[case(NO_DUNDER_PYTHON, None)]
+    #[case(VALID_DUNDER_PYTHON, py_version())]
+    fn dunder_python_specified(#[case] src: &str, #[case] expected: Option<Version>) {
+        let result = check_for_specified_py_vers(src);
+        assert_eq!(result, expected)
+    }
 }
